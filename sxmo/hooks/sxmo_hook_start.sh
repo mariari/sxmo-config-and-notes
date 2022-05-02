2720bcd0cd54f2f04d93e3fe5055e32154947c1f
mariari <mariari@protonmail.com>
2022-05-01 20:08:59 -0500

    Add SXMO config and add notes for what issues I've faced so far

---
+++ sxmo/hooks/sxmo_hook_start.sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+# configversion: 0ba688a17786672d5383dd3d3b5aa748
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright 2022 Sxmo Contributors
+
+# include common definitions
+# shellcheck source=scripts/core/sxmo_common.sh
+. sxmo_common.sh
+
+# in case of weird crash
+echo "unlock" > "$SXMO_STATE"
+[ -f "$SXMO_UNSUSPENDREASONFILE" ] && rm -f "$SXMO_UNSUSPENDREASONFILE"
+
+# Create xdg user directories, such as ~/Pictures
+xdg-user-dirs-update
+
+# Play a funky startup tune if you want (disabled by default)
+#mpv --quiet --no-video ~/welcome.ogg &
+
+sxmo_daemons.sh start daemon_manager superd -v
+
+sleep 2 # let time to superd to start correctly
+
+case "$SXMO_WM" in
+	sway)
+		superctl start mako
+		superctl start sxmo_wob
+		superctl start sxmo_menumode_toggler
+		;;
+	dwm)
+		superctl start dunst
+
+		# Auto hide cursor with touchscreen, Show it with a mouse
+		if command -v "unclutter-xfixes" > /dev/null; then
+			set -- unclutter-xfixes
+		else
+			set -- unclutter
+		fi
+		superctl start "$1"
+
+		# Set a pretty wallpaper
+		feh --bg-fill /usr/share/sxmo/background.jpg
+
+		superctl start autocutsel
+		superctl start autocutsel-primary
+		superctl start sxmo-x11-status
+		;;
+esac
+
+if [ -f "${SXMO_MMS_BASE_DIR:-"$HOME"/.mms/modemmanager}/mms" ]; then
+	superctl start mmsd
+fi
+
+if [ -f "${SXMO_VVM_BASE_DIR:-"$HOME"/.vvm/modemmanager}/vvm" ]; then
+	superctl start vvmd
+fi
+
+# Start the desktop widget (e.g. clock)
+superctl start sxmo_desktop_widget
+
+# Periodically update some status bar components
+sxmo_hook_statusbar.sh all
+sxmo_daemons.sh start statusbar_periodics sxmo_run_periodically.sh 55 \
+	sxmo_hook_statusbar.sh periodics
+
+# Monitor the battery
+superctl start sxmo_battery_monitor
+
+# It watch network changes and update the status bar icon by example
+superctl start sxmo_networkmonitor
+
+# The daemon that display notifications popup messages
+superctl start sxmo_notificationmonitor
+
+# startup the gesture control
+sxmo_hook_lisgdstart.sh
+
+# To setup initial lock state
+sxmo_hook_unlock.sh
+
+superctl start pipewire
+superctl start pipewire-pulse
+superctl start wireplumber
+
+# Verify modemmanager and eg25-manager are running
+if ! sxmo_modemdaemons.sh status; then
+	sxmo_notify_user.sh --urgency=critical "Warning! Modem daemons are not running."
+else
+
+	(
+		sleep 5 # let some time to pipewire
+		superctl start callaudiod
+
+		# Turn on the dbus-monitors for modem-related tasks
+		sxmo_daemons.sh start modem_monitor sxmo_modemmonitor.sh
+	) &
+
+	# Prevent crust for 120s if this is a reboot (uptime < 3mins)
+	if [ "$(cut -d '.' -f1 < /proc/uptime)" -lt 180 ]; then
+		sxmo_daemons.sh start modem_nocrust sleep 120
+	fi
+fi
+
+sxmo_migrate.sh state || sxmo_notify_user.sh --urgency=critical \
+	"Config needs migration" "$? file(s) in your sxmo configuration are out of date and disabled - using defaults until you migrate (run sxmo_migrate.sh)"
---
+++ sxmo/hooks/sxmo_hook_statusbar.sh
@@ -0,0 +1,303 @@
+#!/bin/sh
+# configversion: db937894ff03f4cb447c5f2da3e83c6b
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright 2022 Sxmo Contributors
+
+# we disable shellcheck SC2154 (unreferenced variable used)
+# shellcheck disable=SC2154
+
+# include common definitions
+# shellcheck source=configs/default_hooks/sxmo_hook_icons.sh
+. sxmo_hook_icons.sh
+# shellcheck source=scripts/core/sxmo_common.sh
+. sxmo_common.sh
+
+VPNDEVICE="$(nmcli con show 2>/dev/null | grep -E 'wireguard|vpn' | awk '{ print $1 }')"
+
+set_time() {
+	date "+${SXMO_STATUS_DATE_FORMAT:-%H:%M}" | head -c -1 | sxmo_status.sh add 99-time
+}
+
+set_state() {
+	if grep -q unlock "$SXMO_STATE"; then
+		sxmo_status.sh del 0-state
+	else
+		tr '[:lower:]' '[:upper:]' < "$SXMO_STATE" | sxmo_status.sh add 0-state
+	fi
+}
+
+set_call_duration() {
+	if ! pgrep sxmo_modemcall.sh > /dev/null; then
+		sxmo_status.sh del 0-call-duration
+		return
+	fi
+
+	NOWS="$(date +"%s")"
+	CALLSTARTS="$(date +"%s" -d "$(
+		grep -aE 'call_start|call_pickup' "$XDG_DATA_HOME"/sxmo/modem/modemlog.tsv |
+		tail -n1 |
+		cut -f1
+	)")"
+	CALLSECONDS="$(printf "%s - %s" "$NOWS" "$CALLSTARTS" | bc)"
+	printf "%ss " "$CALLSECONDS" | sxmo_status.sh add 5-call-duration
+}
+
+_modem() {
+	MMCLI="$(mmcli -m any -J 2>/dev/null)"
+	MODEMSTATUS=""
+
+	if [ -z "$MMCLI" ]; then
+		printf "%s" "$icon_cls"
+	else
+		MODEMSTATUS="$(printf %s "$MMCLI" | jq -r .modem.generic.state)"
+		case "$MODEMSTATUS" in
+			locked)
+				printf "%s" "$icon_plk"
+				;;
+			initializing)
+				printf "I"
+				;;
+			disabled) # low power state
+				printf "%s" "$icon_mdd"
+				;;
+			disabling)
+				printf ">%s" "$icon_mdd"
+				;;
+			enabling) # modem enabled but neither registered (cell) nor connected (data)
+				printf ">%s" "$icon_ena"
+				;;
+			enabled)
+				printf "%s" "$icon_ena"
+				;;
+			searching) # i.e. registering
+				printf "%s" "$icon_dot"
+				;;
+			registered|connected|connecting|disconnecting)
+				MODEMSIGNAL="$(printf %s "$MMCLI" | jq -r '.modem.generic."signal-quality".value')"
+				if [ "$MODEMSIGNAL" -lt 20 ]; then
+					printf ""
+				elif [ "$MODEMSIGNAL" -lt 40 ]; then
+					printf ""
+				elif [ "$MODEMSIGNAL" -lt 60 ]; then
+					printf ""
+				elif [ "$MODEMSIGNAL" -lt 80 ]; then
+					printf ""
+				else
+					printf ""
+				fi
+				;;
+			*)
+				# FAILED, UNKNOWN
+				# see https://www.freedesktop.org/software/ModemManager/doc/latest/ModemManager/ModemManager-Flags-and-Enumerations.html#MMModemState
+				sxmo_log "WARNING: MODEMSTATUS: $MODEMSTATUS"
+				printf "<%s>" "$MODEMSTATUS"
+				;;
+		esac
+	fi
+
+	case "$MODEMSTATUS" in
+		"connected")
+			printf " "
+			USEDTECHS="$(printf %s "$MMCLI" | jq -r '.modem.generic."access-technologies"[]')"
+			case "$USEDTECHS" in
+				*5gnr*)
+					printf 5g # no icon yet
+					;;
+				*lte*)
+					printf 4g # ﰒ is in the bad range
+					;;
+				*umts*|*hsdpa*|*hsupa*|*hspa*|*1xrtt*|*evdo0*|*evdoa*|*evdob*)
+					printf 3g # ﰑ is in the bad range
+					;;
+				*edge*)
+					printf E
+					;;
+				*pots*|*gsm*|*gprs*)
+					printf 2g # ﰐ is in the bad range
+					;;
+				*)
+					sxmo_log "WARNING: USEDTECHS: $USEDTECHS"
+					printf "<%s>" "$USEDTECHS"
+					;;
+			esac
+			;;
+		"connecting")
+			printf " >>"
+			;;
+		"disconnecting")
+			printf " <<"
+			;;
+	esac
+}
+
+set_modem() {
+	_modem | sxmo_status.sh add 10-modem-status
+}
+
+set_modem_monitor() {
+	if sxmo_daemons.sh running modem_monitor -q; then
+		sxmo_status.sh add 20-modem-monitor-status "$icon_mod"
+	else
+		sxmo_status.sh del 20-modem-monitor-status
+	fi
+}
+
+set_wifi() {
+	case "$(cat "/sys/class/net/$2/operstate")" in
+		"up") sxmo_status.sh add "30-network-$2-status" "$icon_wif" ;;
+		*) sxmo_status.sh del "30-network-$2-status" ;;
+	esac
+}
+
+set_vpn() {
+	if nmcli -g GENERAL.STATE device show "$2" | grep connected > /dev/null; then
+		sxmo_status.sh add "30-network-$2-status" "$icon_key"
+	else
+		sxmo_status.sh del "30-network-$2-status"
+	fi
+}
+
+# $1: type (reported by nmcli)
+# $2: interface name
+set_network() {
+	case "$1" in
+		wifi) set_wifi "$@" ;;
+		wireguard|vpn) set_vpn "$@" ;;
+		# the type will be empty if the interface disappeared
+		"") sxmo_status.sh del "30-network-$2-status" ;;
+	esac
+}
+
+_battery() {
+	count=0 # if multiple batteries, add space between them
+	for power_supply in /sys/class/power_supply/*; do
+		if [ "$(cat "$power_supply"/type)" = "Battery" ]; then
+			if [ -e "$power_supply"/capacity ]; then
+				PCT="$(cat "$power_supply"/capacity)"
+			elif [ -e "$power_supply"/charge_now ]; then
+				CHARGE_NOW="$(cat "$power_supply"/charge_now)"
+				CHARGE_FULL="$(cat "$power_supply"/charge_full_design)"
+				PCT="$(printf "scale=2; %s / %s * 100\n" "$CHARGE_NOW" "$CHARGE_FULL" | bc | cut -d'.' -f1)"
+			else
+				continue
+			fi
+
+			if [ "$count" -gt 0 ]; then
+				printf " "
+			fi
+			count=$((count+1))
+
+			if [ -e "$power_supply"/status ]; then
+				# The status is not always given for the battery device.
+				# (sometimes it's linked to the charger device).
+				BATSTATUS="$(cut -c1 "$power_supply"/status)"
+			fi
+
+			# Treat 'Full' status as same as 'Charging'
+			if [ "$BATSTATUS" = "C" ] || [ "$BATSTATUS" = "F" ]; then
+				if [ "$PCT" -lt 20 ]; then
+					printf ""
+				elif [ "$PCT" -lt 30 ]; then
+					printf ""
+				elif [ "$PCT" -lt 40 ]; then
+					printf ""
+				elif [ "$PCT" -lt 60 ]; then
+					printf ""
+				elif [ "$PCT" -lt 80 ]; then
+					printf ""
+				elif [ "$PCT" -lt 90 ]; then
+					printf ""
+				else
+					printf ""
+				fi
+			else
+				if [ "$PCT" -lt 10 ]; then
+					printf ""
+				elif [ "$PCT" -lt 20 ]; then
+					printf ""
+				elif [ "$PCT" -lt 30 ]; then
+					printf ""
+				elif [ "$PCT" -lt 40 ]; then
+					printf ""
+				elif [ "$PCT" -lt 50 ]; then
+					printf ""
+				elif [ "$PCT" -lt 60 ]; then
+					printf ""
+				elif [ "$PCT" -lt 70 ]; then
+					printf ""
+				elif [ "$PCT" -lt 80 ]; then
+					printf ""
+				elif [ "$PCT" -lt 90 ]; then
+					printf ""
+				else
+					printf ""
+				fi
+			fi
+
+			[ -z "$SXMO_BAR_HIDE_BAT_PER" ] && printf " %s%%" "$PCT"
+		fi
+	done
+}
+
+set_battery() {
+	 _battery | sxmo_status.sh add 40-battery-status
+}
+
+_volume() {
+	case "$(sxmo_audio.sh device get 2>/dev/null)" in
+		Speaker|"")
+			# nothing for default or pulse devices
+			;;
+		Headphones)
+			printf "%s " "$icon_hdp"
+			;;
+		Earpiece)
+			printf "%s " "$icon_ear"
+			;;
+	esac
+
+	VOL="$(sxmo_audio.sh vol get)"
+	if [ -z "$VOL" ] || [ "$VOL" -eq 0 ]; then
+		printf "%s" "$icon_mut"
+	elif [ "$VOL" -gt 66 ]; then
+		printf "%s" "$icon_spk"
+	elif [ "$VOL" -gt 33 ]; then
+		printf "%s" "$icon_spm"
+	elif [ "$VOL" -gt 0 ]; then
+		printf "%s" "$icon_spl"
+	fi
+}
+
+set_volume() {
+	 _volume | sxmo_status.sh add 50-volume
+}
+
+case "$1" in
+	network)
+		shift
+		set_network "$@"
+		;;
+	time|call_duration|modem|modem_monitor|battery|volume|state)
+		set_"$1"
+		;;
+	periodics|state_change) # 55 s loop and screenlock triggers
+		set_time
+		set_modem
+		set_battery
+		set_state
+		;;
+	all)
+		sxmo_status.sh reset
+		set_time
+		set_call_duration
+		set_modem
+		set_modem_monitor
+		set_battery
+		set_volume
+		set_state
+		;;
+	*)
+		exit # swallow it !
+		;;
+esac
+
