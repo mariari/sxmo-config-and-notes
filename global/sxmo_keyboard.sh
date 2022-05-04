0b47a9a5bd1e68c7e92206c259fb8a15081d614f
mariari <mariari@protonmail.com>
2022-05-04 16:15:52 -0500

    Got rest of my upload selection in

---
+++ global/sxmo_keyboard.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright 2022 Sxmo Contributors
+
+# include common definitions
+# shellcheck source=scripts/core/sxmo_common.sh
+. sxmo_common.sh
+
+isopen() {
+	if [ -z "$KEYBOARD" ]; then
+		exit 0 # ssh/tty usage by example
+	fi
+	pidof "$KEYBOARD" > /dev/null
+}
+
+open() {
+	if [ -n "$KEYBOARD" ]; then
+		#Note: KEYBOARD_ARGS is not quoted by design as it may includes a pipe and further tools
+		# shellcheck disable=SC2086
+		isopen || eval "$KEYBOARD" $KEYBOARD_ARGS &
+	fi
+}
+
+close() {
+	if [ -n "$KEYBOARD" ]; then # avoid killing everything !
+		pkill "$KEYBOARD"
+	fi
+}
+
+if [ "$1" = "toggle" ]; then
+	close || open
+elif [ "$1" = "close" ]; then
+	if isopen; then
+		close
+	fi
+elif [ "$1" = "isopen" ]; then
+	isopen || exit 1
+else
+	open
+fi
---
+++ global/sxmo_screenshot.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+# scrot refuses to work with double quotes
+# shellcheck disable=SC2016
+
+set -e
+
+exitMsg() {
+	printf "%s\n" "$1" > /dev/stderr
+	notify-send "$1"
+	exit 1
+}
+
+commandExists() {
+	command -v "$1" > /dev/null
+}
+
+swayscreenshot() {
+	commandExists grim || exitMsg "grim command must be available to take a screenshot."
+
+	if [ "$1" = selection ]; then
+		commandExists slurp || exitMsg "slurp command must be available to make a selection."
+		notify-send "select an area"
+		set -- grim -g "$(slurp)"
+	else
+		set -- grim
+	fi
+
+	"$@" "$FILENAME"
+}
+
+xorgscreenshot() {
+	commandExists scrot || exitMsg "scrot command must be available to take a screenshot"
+	if [ "$1" = "selection" ]; then
+		notify-send 'select an area'
+		set -- scrot -d 1 -q 1 -s
+	else
+		set -- scrot -d 1 -q 1
+	fi
+
+	"$@" "$FILENAME"
+}
+
+screenshot() {
+	case "$SXMO_WM" in
+		sway)
+			swayscreenshot "$@"
+			;;
+		dwm)
+			xorgscreenshot "$@"
+			;;
+		*)
+			exitMsg "We dont know the WM, cannot screenshot."
+			;;
+	esac
+}
+
+filepathoutput() {
+	printf %s "$FILENAME"
+	case "$SXMO_WM" in
+		sway)
+			wl-copy "$FILENAME"
+			;;
+		dwm)
+			printf %s "$FILENAME" | xsel -b -i
+			;;
+	esac
+}
+
+FILENAME="${SXMO_SCREENSHOT_DIR:-$HOME/Pictures/screenshot/$(date +%Y-%m-%d-%T).png}"
+
+screenshot "$@"
+filepathoutput
